1672bd71690e1564a8e586ebd62e110ec746cfd9
00MRX00 <17515560@mail.ru>
2020-07-03 21:31:35 +0300

    timezone bugs fixed

--- EventService/emails/utils.py
+++ EventService/emails/utils.py
@@ -4,12 +4,11 @@ sys.path.append('../')
 from events.models import Event
 from django.contrib.auth.models import User
 from datetime import datetime
-import pytz
 
 def checkDate():
     emails = []
     for event in Event.objects.all():
-        if not event.done and (event.eventDate - datetime.now(tz=pytz.timezone('Europe/Moscow'))).seconds/60/60 <= 1:
+        if not event.done and (event.eventDate - datetime.now()).seconds/60/60 <= 1:
             emails.append({
                 "title": event.title,
                 "content": event.content,
